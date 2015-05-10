8133d8511cf21423955f70d80938b68d6a7fc271
buptjz <buptjz@gmail.com>
2015-05-10 20:55:48 +0800

    init

---
+++ SidebarDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SidebarDemo
+//
+//  Created by wangjz on 15/5/10.
+//  Copyright (c) 2015年 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
