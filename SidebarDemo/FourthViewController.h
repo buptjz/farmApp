1da30de6878b43993c6d4910bbd6b0c818dc9567
buptjz <buptjz@gmail.com>
2015-05-16 21:36:16 +0800

    添加了第三页和第四页

---
+++ SidebarDemo/FourthViewController.h
@@ -0,0 +1,13 @@
+//
+//  FourthViewController.h
+//  SidebarDemo
+//
+//  Created by WangJZ on 15/5/16.
+//  Copyright (c) 2015年 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FourthViewController : UIViewController
+
+@end
